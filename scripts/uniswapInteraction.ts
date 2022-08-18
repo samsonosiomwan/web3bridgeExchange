6627b786dd82588cd2708e0a7f7abd15867c64ce
mac <samsonosiomwan@gmail.com>
2022-08-18 20:37:10 +0100

    .

--- scripts/uniswapInteraction.ts
+++ scripts/uniswapInteraction.ts
@@ -9,9 +9,42 @@ async function main() {
     const UNIRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
     const amountOut = ethers.utils.parseUnits("4000", "18");
 
-    const provider = ethers.getDefaultProvider();
+    const USDCHolder = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621";
+    await helpers.impersonateAccount(USDCHolder);
+    const impersonatedSigner = await ethers.getSigner(USDCHolder);
 
+    const USDC = await ethers.getContractAt(
+        "IERC20",
+        USDCAddress,
+        impersonatedSigner
+      );
+      const DAI = await ethers.getContractAt("IERC20", DAIAddress);
+      const ROUTER = await ethers.getContractAt(
+        "IUniswap",
+        UNIRouter,
+        impersonatedSigner
+      );
+
+      await USDC.approve(UNIRouter, amountOut);
+
+  
+      const usdcBal = await USDC.balanceOf(USDCHolder);
+      const daiBal = await DAI.balanceOf(USDCHolder);
+
+        console.log(`USDC balance: ${usdcBal.toString()}`);
+        console.log(`DAI balance: ${daiBal.toString()}`);
+      await ROUTER.swapTokensForExactTokens(
+        amountOut, 
+        ethers.utils.parseUnits("10", "18"), 
+        [USDCAddress, DAIAddress],
+        USDCHolder,
+        Math.floor(Date.now() /1000) + (60 * 10)
+      );
     
+      const usdtBalAfter = await USDC.balanceOf(USDCHolder);
+      const daiBalAfter = await DAI.balanceOf(USDCHolder);
+  
+      console.log("balance after swap", usdtBalAfter, daiBalAfter);
 }
 
 
